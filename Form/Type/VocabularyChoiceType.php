aa7e94030907517382cf9ad1add917984e89b8ae
Ryabenko Sergey <ryabenko.sergey@gmail.com>
2014-01-14 18:19:58 +0400

    VocabulatyChoice type fix

--- Form/Type/VocabularyChoiceType.php
+++ Form/Type/VocabularyChoiceType.php
@@ -8,6 +8,8 @@ use NordUa\VocabularyBundle\Service\VocabularyService;
 use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 
@@ -17,23 +19,30 @@ class VocabularyChoiceType extends ChoiceType
   /** @var VocabularyService */
   protected $vs;
 
+  private $cachedChoices;
+
   public function __construct($vs) {
     $this->vs = $vs;
   }
 
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
-    $values = [];
-    $labels = [];
-    /* @var $doc Vocabulary */
-    foreach ((array)$this->vs->getDocs($options['vocabulary']) as $doc) {
-      $values[] = $doc->getSlug();
-      $labels[] = $doc->getValue();
-    }
+    $options['choice_list'] = $this->getChoices($options);
 
-    $options['choice_list'] = new ChoiceList($values, $labels);
     parent::buildForm($builder, $options);
   }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildView(FormView $view, FormInterface $form, array $options)
+  {
+    $options['choice_list'] = $this->getChoices($options);
+
+    parent::buildView($view, $form, $options);
+  }
+
 
   public function setDefaultOptions(OptionsResolverInterface $resolver)
   {
@@ -45,6 +54,27 @@ class VocabularyChoiceType extends ChoiceType
   public function getName()
   {
     return "vocabulary_choice";
+  }
+
+  /**
+   * @param array $options
+   * @return ChoiceList
+   */
+  private function getChoices(array $options)
+  {
+    if (is_null($this->cachedChoices)) {
+      $values = [];
+      $labels = [];
+      /* @var $doc Vocabulary */
+      foreach ((array)$this->vs->getDocs($options['vocabulary']) as $doc) {
+        $values[] = $doc->getSlug();
+        $labels[] = $doc->getValue();
+      }
+
+      $this->cachedChoices = new ChoiceList($values, $labels);
+    }
+
+    return $this->cachedChoices;
   }
 
 }
